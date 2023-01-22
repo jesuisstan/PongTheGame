d363fe177a67279f2d063236b1220e99bcec4660
dtanigaw <dtanigaw@student.42.fr>
2023-01-22 14:53:24 +0100

    All containers work + nodemon for backend (auto refresh)

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    // res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-}).listen(3080, '127.0.0.1');
+})
+
+server.listen(process.env.BACKEND_PORT || 3080);
 
 console.log('Server running at http://127.0.0.1:3080/');
