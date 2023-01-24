859e8cc7a55051e738d458b7040a70358a8c37f4
dtanigaw <dtanigaw@student.42.fr>
2023-01-24 19:03:27 +0100

    Add env to front

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<string | number>('BACKEND_PORT');
+  const port = configService.get<string>('BACKEND_PORT');
   await app.listen(port || '');
 }
 bootstrap();
