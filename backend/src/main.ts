b612f752b61efda57dffb52e379495035af48b82
dtanigaw <dtanigaw@student.42.fr>
2023-01-25 20:29:33 +0100

    Before merge

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-// import { Injectable } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
