3885bab9f5a1ef5364b1556d5ddff7bc77d89984
Benjamin BRASSART <bbrassar@student.42.fr>
2023-01-31 13:58:55 +0100

    Add some swagger documentation on routes

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { UserService } from './user.service';
 
 @Controller('/user')
@@ -7,8 +14,19 @@ export class UserController {
 
   // https://stackoverflow.com/a/71671007
   @Get('/:id(\\d+)')
-  async getUserById(@Param('id') id: string) {
-    const user = await this.users.findUserById(parseInt(id));
+  @ApiOperation({
+    summary: 'Find a user by its id',
+    parameters: [{ name: 'id', in: 'path' }],
+    responses: {
+      '404': {
+        description: 'user with such id was not found',
+      },
+    },
+  })
+  async getUserById(
+    @Param('id', new ParseIntPipe({ errorHttpStatusCode: 400 })) id: number,
+  ) {
+    const user = await this.users.findUserById(id);
 
     if (user === null) throw new NotFoundException();
     return user;
