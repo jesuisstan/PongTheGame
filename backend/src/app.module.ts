c4b98dde38c67f3ed41bdf3cad15cb64ecfe1fd9
Benjamin BRASSART <bbrassar@student.42.fr>
2023-01-31 07:02:32 +0100

    Add match module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { StatusController } from 'src/status/status.controller';
 import { AuthModule } from './auth/auth.module';
+import { MatchModule } from './match/match.module';
 import { PrismaService } from './prisma.service';
 import { UserModule } from './user/user.module';
 
@@ -13,6 +14,7 @@ import { UserModule } from './user/user.module';
     }),
     AuthModule,
     UserModule,
+    MatchModule,
   ],
   providers: [PrismaService],
   controllers: [StatusController],
