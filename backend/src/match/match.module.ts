b85e9fd07a3cbbbd9f73feab269e4844317cd288
Benjamin BRASSART <bbrassar@student.42.fr>
2023-01-31 07:10:47 +0100

    Import prisma module in match module

--- backend/src/match/match.module.ts
+++ backend/src/match/match.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { MatchService } from './match.service';
 
 @Module({
   providers: [MatchService],
+  imports: [PrismaModule],
 })
 export class MatchModule {}
