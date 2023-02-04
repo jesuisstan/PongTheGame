b64e1fd511819d5f766b924414b3563a7151a74a
ichbinstannis <stan.krivtsov@gmail.com>
2023-02-04 03:14:16 +0100

    profile card adjusted

--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -1,5 +1,26 @@
-const NotFound = () => {
-  return <h1>Not Found</h1>;
+import { useNavigate } from 'react-router-dom';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ButtonPong from './UI/ButtonPong';
+
+const NotFound: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Not found</h1>
+      <ButtonPong
+        text="Back"
+        onClick={() => navigate(-1)}
+        startIcon={<ArrowBackIosIcon />}
+      />
+      <ButtonPong
+        text="Home"
+        onClick={() => navigate('/')}
+        endIcon={<ArrowForwardIosIcon />}
+      />
+    </div>
+  );
 };
 
 export default NotFound;
