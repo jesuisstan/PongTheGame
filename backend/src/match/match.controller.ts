874fcb78220e6a1cfc1c7c1784fbf21c67210e9a
Benjamin BRASSART <bbrassar@student.42.fr>
2023-02-10 01:51:19 +0100

    Remove match CRUD

--- backend/src/match/match.controller.ts
+++ backend/src/match/match.controller.ts
@@ -1,20 +1,15 @@
 import {
-  BadRequestException,
-  Body,
   Controller,
   Get,
   NotFoundException,
   Param,
   ParseIntPipe,
-  Post,
   UseGuards,
-  ValidationPipe,
 } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { User } from '@prisma/client';
 import { IsAuthenticatedGuard } from 'src/auth/auth.guard';
 import { SessionUser } from 'src/decorator/session-user.decorator';
-import { CreateMatchDTO } from 'src/match/dto/createMatch.dto';
 import { MatchService } from 'src/match/match.service';
 import { UserService } from 'src/user/user.service';
 
@@ -45,33 +40,5 @@ export class MatchController {
 
     if (match === null) throw new NotFoundException();
     return match;
-  }
-
-  @Post('/')
-  @UseGuards(IsAuthenticatedGuard)
-  @ApiOperation({
-    summary: 'Create a new match that opposes two users',
-  })
-  async createMatch(@Body(ValidationPipe) body: CreateMatchDTO) {
-    const { user1: userId1, user2: userId2 } = body;
-
-    if (userId1 === userId2) {
-      throw new BadRequestException('user1 and user2 have the same value');
-    }
-
-    const ids = [userId1, userId2];
-    const users = await this.users.findUsersById(...ids);
-    const notFoundIds = users
-      .map((user, index) => {
-        if (user === null) return ids[index];
-        return null;
-      })
-      .filter((id) => id !== null);
-
-    if (notFoundIds.length !== 0) {
-      return new NotFoundException(notFoundIds);
-    }
-
-    return this.matches.createMatch(users[0]!!, users[1]!!);
   }
 }
