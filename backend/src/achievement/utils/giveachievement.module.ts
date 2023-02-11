b5d3ac4644086247cfbc70a5b9ffffa3522d2da1
Florian CATINAUD <fcatinau@e2r11p22.clusters.42paris.fr>
2023-02-11 15:10:25 +0100

    Little modif with a route

--- backend/src/achievement/utils/giveachievement.module.ts
+++ backend/src/achievement/utils/giveachievement.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { giveAchievementService } from './giveachievement.service';
+import { giveAchievementService } from 'src/achievement/utils/giveachievement.service';
 
 @Module({
 	providers : [giveAchievementService],
