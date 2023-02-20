802f8907ad1769eb9d7bbc38f15d34b0b50d01a6
Benjamin Brassart <bbrassar@student.42.fr>
2023-02-20 03:34:06 +0100

    Fix messy merge and eslint errors on everything Fixes #83

--- backend/src/achievement/utils/giveachievement.service.ts
+++ backend/src/achievement/utils/giveachievement.service.ts
@@ -1,170 +1,168 @@
-import { PrismaService } from "src/prisma/prisma.service";
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class giveAchievementService {
-	constructor (private prisma : PrismaService){}
+  constructor(private prisma: PrismaService) {}
 
-	async fisrtLogin(user : Express.User){
-		await this.prisma.achievement.update({
-			where : {
-				Title : "First Login",
-			},
-			data : {
-				userId : user.id,
-			},
-		});
-		this.collector(user);
-	}
+  async fisrtLogin(user: User) {
+    await this.prisma.achievement.update({
+      where: {
+        Title: 'First Login',
+      },
+      data: {
+        userId: user.id,
+      },
+    });
+    this.collector(user);
+  }
 
-	async custom(user : Express.User){ // TODO add this in the right request
-		await this.prisma.achievement.update({
-			where : {
-				Title : "Change Avatar",
-			},
-			data : {
-				userId : user.id,
-			},
-		});
-		this.collector(user);
-	}
+  async custom(user: User) {
+    // TODO add this in the right request
+    await this.prisma.achievement.update({
+      where: {
+        Title: 'Change Avatar',
+      },
+      data: {
+        userId: user.id,
+      },
+    });
+    this.collector(user);
+  }
 
-	// async playGame(user : Express.User){
-	// 	const nb_game = 0; //= await // TODO get nb game played
-	
-	// 	if (nb_game == 1)
-	// 	{
-	// 		await this.prisma.achievement.update({
-	// 			where : {
-	// 				Title : "First Game",
-	// 			},
-	// 			data : {
-	// 				userId : user.id,
-	// 			},
-	// 		});
-	// 	}
-	// 	else if (nb_game == 10)
-	// 	{
-	// 		await this.prisma.achievement.update({
-	// 			where : {
-	// 				Title : "Play 10 Games",
-	// 			},
-	// 			data : {
-	// 				userId : user.id,
-	// 			},
-	// 		});
-	// 	}
-	// 	else if (nb_game == 100)
-	// 	{
-	// 		await this.prisma.achievement.update({
-	// 			where : {
-	// 				Title : "Play 100 Games",
-	// 			},
-	// 			data : {
-	// 				userId : user.id,
-	// 			},
-	// 		});
-	// 	}
-	// }
+  // async playGame(user : User){
+  // 	const nb_game = 0; //= await // TODO get nb game played
 
-	// async winGame(user : Express.User){
-	// 	const gameWin = 0;//= await ;// TODO do the request
+  // 	if (nb_game == 1)
+  // 	{
+  // 		await this.prisma.achievement.update({
+  // 			where : {
+  // 				Title : "First Game",
+  // 			},
+  // 			data : {
+  // 				userId : user.id,
+  // 			},
+  // 		});
+  // 	}
+  // 	else if (nb_game == 10)
+  // 	{
+  // 		await this.prisma.achievement.update({
+  // 			where : {
+  // 				Title : "Play 10 Games",
+  // 			},
+  // 			data : {
+  // 				userId : user.id,
+  // 			},
+  // 		});
+  // 	}
+  // 	else if (nb_game == 100)
+  // 	{
+  // 		await this.prisma.achievement.update({
+  // 			where : {
+  // 				Title : "Play 100 Games",
+  // 			},
+  // 			data : {
+  // 				userId : user.id,
+  // 			},
+  // 		});
+  // 	}
+  // }
 
-	// 	if (gameWin == 1)
-	// 	{
-	// 		await this.prisma.achievement.update({
-	// 			where : {
-	// 				Title : "Win One Games",
-	// 			},
-	// 			data : {
-	// 				userId : user.id,
-	// 			},
-	// 		});
-	// 	}
-	// 	else if (gameWin == 10)
-	// 	{
-	// 		await this.prisma.achievement.update({
-	// 			where : {
-	// 				Title : "Win 10 Games",
-	// 			},
-	// 			data : {
-	// 				userId : user.id,
-	// 			},
-	// 		});
-	// 	}
-	// 	else if (gameWin == 100)
-	// 	{
-	// 		await this.prisma.achievement.update({
-	// 			where : {
-	// 				Title : "Win 100 Games",
-	// 			},
-	// 			data : {
-	// 				userId : user.id,
-	// 			},
-	// 		});
-	// 	}
-	// }
+  // async winGame(user : User){
+  // 	const gameWin = 0;//= await ;// TODO do the request
 
-	// async getFriends(user : Express.User){
-	// 	const nb_friends = 0;// TODO do the request
-	// 	if (nb_friends == 1)
-	// 	{
-	// 		await this.prisma.achievement.update({
-	// 			where : {
-	// 				Title : "Get One Friends",
-	// 			},
-	// 			data : {
-	// 				userId : user.id,
-	// 			},
-	// 		});
-	// 	}
-	// 	else if (nb_friends == 42)
-	// 	{
-	// 		await this.prisma.achievement.update({
-	// 			where : {
-	// 				Title : "Get 42 Friends",
-	// 			},
-	// 			data : {
-	// 				userId : user.id,
-	// 			},
-	// 		});
-	// 	}
-	// }
+  // 	if (gameWin == 1)
+  // 	{
+  // 		await this.prisma.achievement.update({
+  // 			where : {
+  // 				Title : "Win One Games",
+  // 			},
+  // 			data : {
+  // 				userId : user.id,
+  // 			},
+  // 		});
+  // 	}
+  // 	else if (gameWin == 10)
+  // 	{
+  // 		await this.prisma.achievement.update({
+  // 			where : {
+  // 				Title : "Win 10 Games",
+  // 			},
+  // 			data : {
+  // 				userId : user.id,
+  // 			},
+  // 		});
+  // 	}
+  // 	else if (gameWin == 100)
+  // 	{
+  // 		await this.prisma.achievement.update({
+  // 			where : {
+  // 				Title : "Win 100 Games",
+  // 			},
+  // 			data : {
+  // 				userId : user.id,
+  // 			},
+  // 		});
+  // 	}
+  // }
 
-	async collector(user : Express.User)
-	{
-		const nb_achievements = await this.prisma.achievement.findMany({
-			where : {
-				userId : user.id,
-			},
-		});
-		if (nb_achievements.length == 10)
-		{
-			await this.prisma.achievement.update({
-				where : {
-					Title : "Achievements everywhere",
-				},
-				data : {
-					userId : user.id,
-				},
-			});
-		}
-		else if (nb_achievements.length == 20)
-		{
-			await this.prisma.achievement.update({
-				where : {
-					Title : "Achievements everywhere * 2",
-				},
-				data : {
-					userId : user.id,
-				},
-			});
-		}
-	}
-	getAchievement(user : Express.User){
-		// playGame(user);
-		// winGame(user);
-		// getFriends(user);
-		this.collector(user);
-	}
+  // async getFriends(user : User){
+  // 	const nb_friends = 0;// TODO do the request
+  // 	if (nb_friends == 1)
+  // 	{
+  // 		await this.prisma.achievement.update({
+  // 			where : {
+  // 				Title : "Get One Friends",
+  // 			},
+  // 			data : {
+  // 				userId : user.id,
+  // 			},
+  // 		});
+  // 	}
+  // 	else if (nb_friends == 42)
+  // 	{
+  // 		await this.prisma.achievement.update({
+  // 			where : {
+  // 				Title : "Get 42 Friends",
+  // 			},
+  // 			data : {
+  // 				userId : user.id,
+  // 			},
+  // 		});
+  // 	}
+  // }
+
+  async collector(user: User) {
+    const nb_achievements = await this.prisma.achievement.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
+    if (nb_achievements.length == 10) {
+      await this.prisma.achievement.update({
+        where: {
+          Title: 'Achievements everywhere',
+        },
+        data: {
+          userId: user.id,
+        },
+      });
+    } else if (nb_achievements.length == 20) {
+      await this.prisma.achievement.update({
+        where: {
+          Title: 'Achievements everywhere * 2',
+        },
+        data: {
+          userId: user.id,
+        },
+      });
+    }
+  }
+  getAchievement(user: User) {
+    // playGame(user);
+    // winGame(user);
+    // getFriends(user);
+    this.collector(user);
+  }
 }
--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,37 +1,36 @@
-import { Injectable, ExecutionContext } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { PassportStrategy } from "@nestjs/passport";
-import { ExtractJwt, Strategy } from "passport-jwt";
-import { PrismaService } from "src/prisma/prisma.service";
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { Request as RequestType } from 'express';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, 'jwt'){
-	constructor (config: ConfigService, private prisma: PrismaService){
-		super({
-			jwtFromRequest : ExtractJwt.fromExtractors([
-				JwtStrategy.fromCookie,
-				ExtractJwt.fromAuthHeaderAsBearerToken(),
-			  ]),// MEMO http://www.passportjs.org/packages/passport-jwt/#extracting-the-jwt-from-the-request
-			ignoreExpiration:false,
-			secretOrKey: config.get('JWT_SECRET'),
-		});
-	}
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService, private prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        JwtStrategy.fromCookie,
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ]), // MEMO http://www.passportjs.org/packages/passport-jwt/#extracting-the-jwt-from-the-request
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
 
-	private static fromCookie(req : RequestType): string | null {
-		if ( req.cookies && 'access_token' in req.cookies) {
-			return req.cookies.access_token;
-		}
-		return null;
-	}
+  private static fromCookie(req: RequestType): string | null {
+    if (req.cookies && 'access_token' in req.cookies) {
+      return req.cookies.access_token;
+    }
+    return null;
+  }
 
-	async validate(payload : {id : number}){
-		const user = await this.prisma.user.findUnique({
-			where :{
-				id : payload.id,
-			}
-		});
-		return user;
-	}
-
+  async validate(payload: { id: number }) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.id,
+      },
+    });
+    return user;
+  }
 }
