22c5119c0cfb9395f32855eb9a19c08b3e489c71
Florian CATINAUD <fcatinau@e2r7p22.clusters.42paris.fr>
2023-02-22 19:27:33 +0100

    Github works + delete the good cookies. Resolved #105

--- backend/src/auth/github/github.controller.ts
+++ backend/src/auth/github/github.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Res, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { User } from '@prisma/client';
 import { Response } from 'express';
 import { giveAchievementService } from 'src/achievement/utils/giveachievement.service';
 import { GithubGuard } from 'src/auth/github/github.guard';
+import { AuthService } from 'src/auth/auth.service';
 import { SessionUser } from 'src/decorator/session-user.decorator';
+import { convertTime } from 'src/utils/time';
 
 // TODO use config
 const CLIENT_URL = 'http://localhost:3000';
@@ -12,7 +14,7 @@ const CLIENT_URL = 'http://localhost:3000';
 @Controller('/auth/github')
 @ApiTags('Authentication/Github')
 export class GithubController {
-  constructor(private give: giveAchievementService) {}
+  constructor(private readonly auth: AuthService, private give: giveAchievementService) {}
 
   @Get('/')
   @ApiOperation({ summary: 'Connect using the Github OAuth2 API' })
@@ -33,7 +35,15 @@ export class GithubController {
   })
   @UseGuards(GithubGuard)
   async callback(@SessionUser() user: User, @Res() res: Response) {
+    if (user === undefined) throw new UnauthorizedException();
     this.give.fisrtLogin(user);
+    const token = await this.auth.signToken(user.id);
+
+    res.cookie('access_token', token, {
+      maxAge: convertTime({
+        minutes: 30,
+      }),
+    });
     return res.redirect(`${CLIENT_URL}/profile`);
   }
 }
