c10b0bee1486c33fce051bcd9c45a2d740d4ebbb
Florian CATINAUD <fcatinau@e1r6p5.clusters.42paris.fr>
2023-02-28 23:20:21 +0100

    Always lint error

--- backend/prisma/user.ts
+++ backend/prisma/user.ts
@@ -1,18 +1,17 @@
 import { User, Prisma } from '@prisma/client';
 import { prisma } from './seed';
 
-export async function insert_admin(){
+export async function insert_admin() {
   console.log('Create admin user');
-  const admin_user: Prisma.UserCreateInput =
-    {
-      username: "Admin",
-      profileId: "admin",
-      provider: "ADMIN",
-      role: "ADMIN",
-    };
-  const user : User = await prisma.user.create({data: admin_user,});
+  const admin_user: Prisma.UserCreateInput = {
+    username: 'Admin',
+    profileId: 'admin',
+    provider: 'ADMIN',
+    role: 'ADMIN',
+  };
+  const user: User = await prisma.user.create({ data: admin_user });
   await prisma.achievement.update({
-    where : {
+    where: {
       Title: 'Be Admin',
     },
     data: {
