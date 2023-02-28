b106717aacf15321599c81e34b43f48d62b4159a
Florian CATINAUD <fcatinau@e1r6p5.clusters.42paris.fr>
2023-02-28 23:17:36 +0100

    Always forget Eslint

--- backend/prisma/user.ts
+++ backend/prisma/user.ts
@@ -2,22 +2,22 @@ import { User, Prisma } from '@prisma/client';
 import { prisma } from './seed';
 
 export async function insert_admin(){
-	console.log('Create admin user');
-	const admin_user: Prisma.UserCreateInput =
-		{
-			username: "Admin",
-			profileId: "admin",
-			provider: "ADMIN",
-			role: "ADMIN",
-		};
-	const user : User = await prisma.user.create({data: admin_user,});
-	await prisma.achievement.update({
-		where : {
-			Title: 'Be Admin',
-		},
-		data: {
-			userId: user.id,
-		},
-	});
-	console.log(`User ${user.username} create at id: ${user.id}`);
+  console.log('Create admin user');
+  const admin_user: Prisma.UserCreateInput =
+    {
+      username: "Admin",
+      profileId: "admin",
+      provider: "ADMIN",
+      role: "ADMIN",
+    };
+  const user : User = await prisma.user.create({data: admin_user,});
+  await prisma.achievement.update({
+    where : {
+      Title: 'Be Admin',
+    },
+    data: {
+      userId: user.id,
+    },
+  });
+  console.log(`User ${user.username} create at id: ${user.id}`);
 }
