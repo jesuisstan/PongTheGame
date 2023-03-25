95f0d02e40095eb595d804ad54ebe6d1669ab38f
Barbara CANO <bcano@e1r1p4.clusters.42paris.fr>
2023-03-25 16:39:41 +0100

    - Css change:   - fix bubble size ..............[ok] - Little modif to timeFromNow ....[ok]

--- frontend/src/components/chat/utils/timeFromNow.ts
+++ frontend/src/components/chat/utils/timeFromNow.ts
@@ -5,25 +5,25 @@ export default function timeFromNow(timestamp: Date) {
   let interval = Math.floor(seconds / 31536000);
   
   if (interval >= 1) {
-    return interval + ' years ago';
+    return interval + ' years';
   }
   interval = Math.floor(seconds / 2592000);
   if (interval >= 1) {
-    return interval + ' months ago';
+    return interval + ' months';
   }
   interval = Math.floor(seconds / 86400);
   if (interval >= 1) {
-    return interval + ' days ago';
+    return interval + ' days';
   }
   interval = Math.floor(seconds / 3600);
   if (interval >= 1) {
-    return interval + ' hours ago';
+    return interval + ' hours';
   }
   interval = Math.floor(seconds / 60);
   if (interval >= 1) {
-    return interval + ' min ago';
+    return interval + ' min';
   }
-  if (seconds < 35) {
+  if (seconds < 25) {
     return 'now';
   }
   return Math.floor(seconds) + ' sec';
