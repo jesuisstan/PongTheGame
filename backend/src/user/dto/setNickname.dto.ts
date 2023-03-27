99ac437b5171391f79694e87317e6fb4955ec389
Benjamin Brassart <bbrassar@student.42.fr>
2023-03-28 01:54:30 +0200

    Change nickname validation regex to alphanumerical characters, dash and underscore Set nickname minimum and maximum length as constants

--- backend/src/user/dto/setNickname.dto.ts
+++ backend/src/user/dto/setNickname.dto.ts
@@ -1,16 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, Length, Matches } from 'class-validator';
+import { IsString, Length, Matches } from 'class-validator';
 
-const NICKNAME_VALIDATION_REGEX = /^[^\[\]<>^$%.\|/?*+() ]+$/;
+const NICKNAME_VALIDATION_REGEX = /^[A-zA-Z0-9_-]+$/;
+const MIN_LENGTH = 3 as const;
+const MAX_LENGTH = 10 as const;
 
 export class SetNicknameDTO {
-  @IsNotEmpty()
-  @Length(3, 10)
+  @IsString()
+  @Length(MIN_LENGTH, MAX_LENGTH)
   @Matches(NICKNAME_VALIDATION_REGEX, { message: 'illegal character' })
   @ApiProperty({
     default: '',
-    minLength: 3,
-    maxLength: 10,
+    minLength: MIN_LENGTH,
+    maxLength: MAX_LENGTH,
     pattern: NICKNAME_VALIDATION_REGEX.toString(),
   })
   nickname: string;
