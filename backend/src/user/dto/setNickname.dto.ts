b2774fa78506d528596892fdf690947c68b21ed4
Benjamin Brassart <bbrassar@student.42.fr>
2023-03-28 02:03:01 +0200

    Change regex to match any character count

--- backend/src/user/dto/setNickname.dto.ts
+++ backend/src/user/dto/setNickname.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString, Length, Matches } from 'class-validator';
 
-const NICKNAME_VALIDATION_REGEX = /^[A-zA-Z0-9_-]+$/;
+const NICKNAME_VALIDATION_REGEX = /^[A-zA-Z0-9_-]*$/;
 const MIN_LENGTH = 3 as const;
 const MAX_LENGTH = 10 as const;
 
