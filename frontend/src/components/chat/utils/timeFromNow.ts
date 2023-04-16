fcdff46745374441ac8087ee8fc688a7fad1d714
Barbara CANO <bcano@e1r4p4.clusters.42paris.fr>
2023-04-16 16:37:47 +0200

    #394 rework timeFromNow and add plurial

--- frontend/src/components/chat/utils/timeFromNow.ts
+++ frontend/src/components/chat/utils/timeFromNow.ts
@@ -4,27 +4,19 @@ export default function timeFromNow(timestamp: Date) {
   const seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
   let interval = Math.floor(seconds / 31536000);
 
-  if (interval >= 1) {
-    return interval + ' years';
-  }
+  if (interval >= 1 && interval < 2) return interval + ' year';
+  else if (interval >= 2) return interval + ' years';
   interval = Math.floor(seconds / 2592000);
-  if (interval >= 1) {
-    return interval + ' months';
-  }
+  if (interval >= 1 && interval < 2) return interval + ' month';
+  else if (interval >= 2) return interval + ' months';
   interval = Math.floor(seconds / 86400);
-  if (interval >= 1) {
-    return interval + ' days';
-  }
+  if (interval >= 1 && interval < 2) return interval + ' day';
+  else if (interval >= 2) return interval + ' days';
   interval = Math.floor(seconds / 3600);
-  if (interval >= 1) {
-    return interval + ' hours';
-  }
+  if (interval >= 1 && interval < 2) return interval + ' hour';
+  else if (interval >= 2) return interval + ' hours';
   interval = Math.floor(seconds / 60);
-  if (interval >= 1) {
-    return interval + ' min';
-  }
-  if (seconds < 25) {
-    return 'now';
-  }
-  return Math.floor(seconds) + ' sec';
+  if (interval >= 1 && interval < 2) return interval + ' min';
+  else if (interval >= 2) return interval + ' mins';
+  else return 'now';
 }
