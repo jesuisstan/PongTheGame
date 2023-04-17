17e4c147ab367ac309fae719850e52f5af40e451
Benjamin Brassart <bbrassar@student.42.fr>
2023-04-17 19:22:47 +0200

    Use current url for socket.io if in production mode (redirection is handled by nginx)

--- frontend/src/contexts/WebsocketContext.ts
+++ frontend/src/contexts/WebsocketContext.ts
@@ -1,6 +1,5 @@
 import { createContext } from 'react';
 import { io, Socket } from 'socket.io-client';
-import { backendUrl } from '../api/axios-instance';
 
 function get_cookie_access_token() {
   const cookies: string[] = document.cookie.split('=');
@@ -8,9 +7,17 @@ function get_cookie_access_token() {
   return cookies[1];
 }
 
-// Create a new websocket connected to the backend port
-export const socket = io(backendUrl, {
-  auth: { token: get_cookie_access_token() }
-});
+const opts = {
+  auth: {
+    token: get_cookie_access_token()
+  }
+};
+
+// // Create a new websocket connected to the backend
+export const socket =
+  process.env.NODE_ENV === 'development'
+    ? io(process.env.REACT_APP_URL_BACKEND, opts)
+    : io(opts);
+
 // Define context with the previous socket
 export const WebSocketContext = createContext<Socket>(socket);
