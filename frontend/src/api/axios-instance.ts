1c8112fb3a8dc368c5c4d0dc60999ac0da94fec7
Benjamin Brassart <bbrassar@student.42.fr>
2023-04-17 19:07:49 +0200

    Use hardcoded backend url when using production mode

--- frontend/src/api/axios-instance.ts
+++ frontend/src/api/axios-instance.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+export const backendUrl =
+  process.env.NODE_ENV === 'development'
+    ? process.env.REACT_APP_URL_BACKEND
+    : '/api';
+
 const backendAPI = axios.create({
-  baseURL: String(process.env.REACT_APP_URL_BACKEND),
+  baseURL: backendUrl,
   withCredentials: true,
   headers: { 'Content-type': 'application/json; charset=UTF-8' }
 });
