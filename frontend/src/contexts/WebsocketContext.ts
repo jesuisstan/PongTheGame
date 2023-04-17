30bab869a0e84c8b2404cecec524a483f3cb257f
Benjamin Brassart <bbrassar@student.42.fr>
2023-04-17 19:08:42 +0200

    Use backendUrl for socket.io

--- frontend/src/contexts/WebsocketContext.ts
+++ frontend/src/contexts/WebsocketContext.ts
@@ -1,17 +1,16 @@
-import { io, Socket } from "socket.io-client";
-import { createContext } from "react";
+import { createContext } from 'react';
+import { io, Socket } from 'socket.io-client';
+import { backendUrl } from '../api/axios-instance';
 
-function get_cookie_access_token()
-{
-    const cookies : string[] = document.cookie.split("=");
-    if (cookies[0] !== "access_token")
-        return (null);
-    return (cookies[1]);
+function get_cookie_access_token() {
+  const cookies: string[] = document.cookie.split('=');
+  if (cookies[0] !== 'access_token') return null;
+  return cookies[1];
 }
 
-// Create a new websocket connected to the backend port 
-export const socket = io(process.env.REACT_APP_URL_BACKEND, {
-    auth : {token : get_cookie_access_token()},
-})
+// Create a new websocket connected to the backend port
+export const socket = io(backendUrl, {
+  auth: { token: get_cookie_access_token() }
+});
 // Define context with the previous socket
-export const WebSocketContext = createContext<Socket>(socket)
+export const WebSocketContext = createContext<Socket>(socket);
