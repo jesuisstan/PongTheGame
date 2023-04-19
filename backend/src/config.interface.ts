c88893030a0de09cfe27803f3ba63cf49cff6b70
Benjamin Brassart <bbrassar@student.42.fr>
2023-04-19 09:42:12 +0200

    Remove callback urls from environment

--- backend/src/config.interface.ts
+++ backend/src/config.interface.ts
@@ -3,11 +3,9 @@ export type NodeEnv = 'production' | 'development';
 export interface Config {
   INTRA42_CLIENT_ID: string;
   INTRA42_CLIENT_SECRET: string;
-  INTRA42_CALLBACK_URL: string;
 
   GITHUB_CLIENT_ID: string;
   GITHUB_CLIENT_SECRET: string;
-  GITHUB_CALLBACK_URL: string;
 
   BACKEND_URL: string;
   FRONTEND_URL: string;
