385d97378f9045d4df90a96134782db430058626
jesuisstan <stan.krivtsov@gmail.com>
2023-05-16 21:25:58 +0200

    tsconfig changed 3

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -78,6 +78,146 @@ describe('Appe@e', () => {
     app.close();
   });
 
+  // describe('All test', async () => {
+  // describe('Match', () => {
+  //   describe('mine', () => {
+  //     it('Should thrown not connected', async () => {
+  //       return pactum.spec().get('/match/mine').expectStatus(401);
+  //     });
+  //     it('Should thrown no match play', async () => {
+  //       const token = await JWT_access(2);
+  //       return pactum.spec().get('/match/mine').withHeaders(
+  //         {
+  //           Authorization : 'Bearer ' + token,
+  //         }).expectStatus(200);
+  //     })
+  //   });
+  //   describe(':id', () => {
+  //     it('should thrown not connected', async () => {
+  //       return pactum.spec().get('/match/0').expectStatus(401);
+  //     });
+  //     it('Should thrown with a bad id', async () => {
+  //       const token = await JWT_access(2);
+  //       return pactum
+  //         .spec()
+  //         .get('/match/0')
+  //         .withHeaders({
+  //           Authorization: 'Bearer ' + token,
+  //         })
+  //         .expectStatus(404); // Why 401
+  //     });
+  //     // it('Should work', async () => { // MEMO Not work because i cant create a Match
+  //     //   const token = await JWT_access(2);
+  //     //   return pactum.spec().get('/match/1').withHeaders({
+  //     //     Authorization : 'Bearer ' + token,
+  //     //   }).expectStatus(200);
+  //     // })
+  //   });
+  // });
+  // describe('Users', () => {
+  //   describe(':id', () => {
+  //     it('Should thrown not connected', async () => {
+  //       return pactum.spec().get('/user/1',).expectStatus(401);
+  //     });
+  //     it('Should thrown with a bad id', async () => {
+  //       const token = await JWT_access(2);
+  //       return pactum
+  //         .spec()
+  //         .get('/user/0')
+  //         .withHeaders({
+  //           Authorization: 'Bearer ' + token,
+  //         })
+  //         .expectStatus(404);
+  //     });
+  //     it('Should work', async () => {
+  //       const token = await JWT_access(2);
+  //       return pactum
+  //         .spec()
+  //         .get('/user/1')
+  //         .withHeaders({
+  //           Authorization: 'Bearer ' + token,
+  //         })
+  //         .expectStatus(200);
+  //     });
+  //   });
+  //   describe('setnickname', () => {
+  //     it('Should thrown not connected', async () => {
+  //       return pactum.spec().patch('/user/setnickname').expectStatus(401);
+  //     });
+  //     it('Should thrown no body', async () => {
+  //       const token = await JWT_access(2);
+  //       return pactum
+  //         .spec()
+  //         .patch('/user/setnickname')
+  //         .withHeaders({
+  //           Authorization: 'Bearer ' + token,
+  //         })
+  //         .expectStatus(400);
+  //     });
+  //     it('Should work', async () => {
+  //       const token = await JWT_access(2);
+  //       return pactum
+  //         .spec()
+  //         .patch('/user/setnickname')
+  //         .withHeaders({
+  //           Authorization: 'Bearer ' + token,
+  //         })
+  //         .withBody({
+  //           nickname: 'test',
+  //         })
+  //         .expectStatus(200);
+  //     });
+  //     it('Should thrown change with same nickname', async () => {
+  //       const token = await JWT_access(2);
+  //       return pactum
+  //         .spec()
+  //         .patch('/user/setnickname')
+  //         .withHeaders({
+  //           Authorization: 'Bearer ' + token,
+  //         })
+  //         .withBody({
+  //           nickname: 'test',
+  //         })
+  //         .expectStatus(400);
+  //     });
+  //     it('', async () => {
+  //       const token = await JWT_access(2);
+  //       return pactum
+  //         .spec()
+  //         .patch('/user/setnickname')
+  //         .withHeaders({
+  //           Authorization: 'Bearer ' + token,
+  //         })
+  //         .withBody({
+  //           nickname: 'Admin2',
+  //         })
+  //         .expectStatus(200);
+  //     });
+  //   });
+  //   // describe('2fa', () => {
+  //   //   it('Should thrown not connected', async () => {
+  //   //     return pactum.spec().patch('/user/2fa').expectStatus(401);
+  //   //   });
+  //   //   // it('Should thrown no body', async () => {// MEMO No error because DTO dont have notempty
+  //   //   //   const token = await JWT_access(2);
+  //   //   //   return pactum.spec().patch('/user/2fa',).withHeaders({
+  //   //   //     Authorization : 'Bearer ' + token,}).expectStatus(400);
+  //   //   // });
+  //   //   it('Should work', async () => {
+  //   //     const token = await JWT_access(2);
+  //   //     return pactum
+  //   //       .spec()
+  //   //       .patch('/user/2fa')
+  //   //       .withHeaders({
+  //   //         Authorization: 'Bearer ' + token,
+  //   //       })
+  //   //       .withBody({
+  //   //         enabled: true,
+  //   //       })
+  //   //       .expectStatus(200);
+  //   //   });
+  //   // });
+  // });
   describe('Achievement', () => {
     const dto: AchievementDTO = {
       Name: 'Test',
